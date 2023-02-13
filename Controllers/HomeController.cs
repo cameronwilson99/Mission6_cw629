12318964f97f86d34622fbb18a149be9d133eb1d
Cameron Wilson <94140904+cameronwilson99@users.noreply.github.com>
2023-02-13 08:39:35 -0700

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using HiltonMovies.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiltonMovies.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieDBContext _movieContext { get; set; }
+
+        // constructor, constructs model context
+        public HomeController(ILogger<HomeController> logger, MovieDBContext data)
+        {
+            _logger = logger;
+            _movieContext = data;
+        }
+
+        // returns the home page
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        // returns the podcast info page
+        public IActionResult Bacon()
+        {
+            return View();
+        }
+
+        // returns the page view for the new movie form
+        [HttpGet]
+        public IActionResult Movies()
+        {
+            return View();
+        }
+
+        // posts the new movie entered by the user to the model database
+        [HttpPost]
+        public IActionResult Movies(MovieDatabase newMovie)
+        {
+            _movieContext.Add(newMovie);
+            _movieContext.SaveChanges();
+
+            return View("Confirmation", newMovie);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/MovieDatabase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiltonMovies.Models
+{
+    public class MovieDatabase
+    {
+        // Initializes the fields required for the database
+
+        // sets MovieId as the key and requires the input for the below
+        [Key]
+        [Required]
+        public int MovieId { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public int Year { get; set; }
+        [Required]
+        public string Director { get; set; }
+        [Required]
+        public string Rating { get; set; }
+        public bool Edited { get; set; }
+        public string LentTo { get; set; }
+        // Sets max character limit to 25
+        [StringLength(25)]
+        public string Notes { get; set; }
+    }
+}
